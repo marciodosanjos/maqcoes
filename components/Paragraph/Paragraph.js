fb6b170706b182272cd9f93c3c701f9686342b4f
Marcio dos Anjos <marciodosanjos@ymail.com>
2024-08-16 10:24:53 +0200

    relative to absolute url

--- components/Paragraph/Paragraph.js
+++ components/Paragraph/Paragraph.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { getTextAlign } from "utils/fonts";
+import { relativeToAbsoluteURls } from "utils/relativeToAbsoluteUrls";
 
 
 
 export const Paragraph = ({ textAlign = "left", content, textColor}) => {
-  return <p className="max-w-5xl mx-auto" style={{color:textColor, textAlign: textAlign}} dangerouslySetInnerHTML={{__html: content}}/>
+  return <p className="max-w-5xl mx-auto" style={{color:textColor, textAlign: textAlign}} dangerouslySetInnerHTML={{__html: relativeToAbsoluteURls(content)}}/>
 };
